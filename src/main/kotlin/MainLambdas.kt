dd60eeeb11031094a66963603795ddaa716d2639
protorxgenera <109683499+protorxgenera@users.noreply.github.com>
2022-12-18 18:10:16 +0200

    commented on lambdas

--- src/main/kotlin/MainLambdas.kt
+++ src/main/kotlin/MainLambdas.kt
@@ -1,6 +1,14 @@
 fun main() {
 
 	// val lambdaFunction: (String, Int) -> Unit = {string: String, int: Int -> codeblock}
+	/**
+	 * 	A lambda expression is stored in a variable and has variable-notation
+	 *
+	 * 	val instead of fun
+	 * 	name used to identify and call the function - the lambda expression is called with the name of the variable, just as a normal function would be (so nameOfFunction(parameters)
+	 *
+	 */
+
 	lineLogger {
 		println("This is really nice boy")
 		println("I get it amigo")
