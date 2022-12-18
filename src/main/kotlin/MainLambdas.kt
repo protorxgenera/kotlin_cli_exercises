cf6c8b9e9e73803651845b715d448f8daad0ae57
protorxgenera <109683499+protorxgenera@users.noreply.github.com>
2022-12-18 18:12:32 +0200

    commented on lambdas (again)

--- src/main/kotlin/MainLambdas.kt
+++ src/main/kotlin/MainLambdas.kt
@@ -1,17 +1,22 @@
 fun main() {
 
 	// val lambdaFunction: (String, Int) -> Unit = {string: String, int: Int -> codeblock}
+
 	/**
-	 * 	A lambda expression is stored in a variable and has variable-notation
+	 *  The following example is a combination of anonymous functions and a lambda expression
+	 * 	A lambda expression is a function inside braces, is stored in a variable and has variable-notation
 	 *
 	 * 	val instead of fun
-	 * 	name used to identify and call the function - the lambda expression is called with the name of the variable, just as a normal function would be (so nameOfFunction(parameters)
-	 *
+	 * 	name used to identify and call the function - the lambda expression is called with the name of the variable, just as a normal function would be (so nameOfFunction(parameters))
+	 * 	type of function - instead of variableName: Int/String/Boolean, we have variableName: (argumentType, argumentType) -> returnType
+	 * 	equal sign (as opposed to a space)
+	 * 	{variable: argumentType, variable: argumentType -> code-block that returns returnType}
+	 * 	the type can be inferred, so the main difference is that the arguments of the function are written inside the curly braces + the arrow sign ->
 	 */
 
 	lineLogger {
 		println("This is really nice boy")
-		println("I get it amigo")
+		println("I git it amigo")
 	}
 
 }
