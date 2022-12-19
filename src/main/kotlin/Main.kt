8c5568c9c70955aa66c7d6c5fa62fafba16b90a6
protorxgenera <109683499+protorxgenera@users.noreply.github.com>
2022-12-19 23:46:57 +0200

    added exercise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
 fun main() {
 
+	// input > n numbers into input, the first number is n
+	// 1..n numbers into an array
+	// app checks if the array is in ascending order
+		// if yes, print YES
+		// if not, print NO
+
+	val index = readln().toInt()
+	val numberArray = mutableListOf<Int>()
+	repeat(index) {
+		numberArray.add(readln().toInt())
+	}
+	var checkIfAscending = true
+
+	for (i in 0 until index - 1) {
+		if (numberArray[i] < numberArray[i + 1]) {
+			continue
+		} else {
+			checkIfAscending = false
+			break
+		}
+	}
+
+	println(if (checkIfAscending) "YES" else "NO")
+
 }
